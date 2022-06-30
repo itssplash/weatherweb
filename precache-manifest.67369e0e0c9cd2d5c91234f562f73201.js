12d575b4ffba6774ec19d358fb22419a48096d58
Abhinav Jangra <51170452+itssplash@users.noreply.github.com>
2022-07-01 01:55:35 +0530

    Updates

---
+++ precache-manifest.67369e0e0c9cd2d5c91234f562f73201.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87dd68c88f5938857b4e34432c59181f",
+    "url": "/weatherweb/index.html"
+  },
+  {
+    "revision": "93de2a860422ff156164",
+    "url": "/weatherweb/static/css/main.323e5292.chunk.css"
+  },
+  {
+    "revision": "17bb53fcdd865cdd9a3b",
+    "url": "/weatherweb/static/js/2.ad3b9c4a.chunk.js"
+  },
+  {
+    "revision": "f0ad72fb0c1c1e4a268423d8869404c9",
+    "url": "/weatherweb/static/js/2.ad3b9c4a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "93de2a860422ff156164",
+    "url": "/weatherweb/static/js/main.a4cf0b54.chunk.js"
+  },
+  {
+    "revision": "620a60067805c2d02d98",
+    "url": "/weatherweb/static/js/runtime-main.1404b1e8.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/weatherweb/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/weatherweb/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
